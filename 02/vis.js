e145c5edbc12700e03ae20b9ed241c93995df25a
Micah Stubbs <micah.stubbs@gmail.com>
2017-04-23 17:30:37 -0400

    lebab.sh

--- 02/vis.js
+++ 02/vis.js
@@ -1,44 +1,42 @@
-    var width = 960,
-        height = 500;
+    const width = 960;
+    const height = 500;
 
-    var force = d3.layout.force()
+    const force = d3.layout.force()
         .charge(-120)
         .linkDistance(30)
         .size([width, height]);
 
-    var forceLabels = d3.layout.force()
+    const forceLabels = d3.layout.force()
         .size([width, height]);
 
-    var svg = d3.select("body").append("svg")
+    const svg = d3.select("body").append("svg")
         .attr("width", width)
         .attr("height", height);
 
-    var tip = d3.tip()
+    const tip = d3.tip()
       .attr('class', 'd3-tip')
       .offset([-10, 0])
-      .html(function(d) {
-        return "<strong>" + d.name + "</strong>   <span style='color:#CCC'>" + d.role + "</span>";
-      });
+      .html(d => `<strong>${d.name}</strong>   <span style='color:#CCC'>${d.role}</span>`);
 
     svg.call(tip);
 
-    d3.csv("org.csv", function(error, org) {
-      var color = d3.scale.category20();
-      var indexMap = getIndexMap(org);
-      var nameMap = getNameMap(org);
-      var links = getLinks(indexMap, nameMap, org);
-      var projects = getProjects(org);
-      var fociMap = getFociMap(projects);
-      var labels = [];
-      var displayedLinks = [];
-      var graph = {
+    d3.csv("org.csv", (error, org) => {
+      const color = d3.scale.category20();
+      const indexMap = getIndexMap(org);
+      const nameMap = getNameMap(org);
+      const links = getLinks(indexMap, nameMap, org);
+      const projects = getProjects(org);
+      const fociMap = getFociMap(projects);
+      let labels = [];
+      let displayedLinks = [];
+      const graph = {
         "links": links,
         "nodes": org
-      }
+      };
 
-      var node = svg.selectAll(".node");
-      var link = svg.selectAll(".link");
-      var label = svg.selectAll(".label");
+      let node = svg.selectAll(".node");
+      let link = svg.selectAll(".link");
+      let label = svg.selectAll(".label");
 
       force
         .nodes(graph.nodes)
@@ -82,7 +80,7 @@
         link = link.data(displayedLinks);
         link.enter().append("line")
           .attr("class", "link")
-          .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+          .style("stroke-width", d => Math.sqrt(d.value));
         link.exit().remove();
         force.links(displayedLinks).start();
 
@@ -90,15 +88,15 @@
         node.enter().append("circle")
             .attr("class", "node")
             .attr("r", 5)
-            .style("fill", function(d) { return color(d.team); })
+            .style("fill", d => color(d.team))
             .call(force.drag)
-            .on("mousedown", function() { d3.event.stopPropagation(); })
+            .on("mousedown", () => { d3.event.stopPropagation(); })
             .on('mouseover', tip.show)
             .on('mouseout', tip.hide);
 
         node.exit().remove();
         node.append("title")
-            .text(function(d) { return d.name; });
+            .text(d => d.name);
         label = svg.selectAll(".label")
           .data(labels);
         label.enter().append("text")
@@ -109,24 +107,24 @@
         label.exit().remove();
 
         forceLabels.nodes(labels).start();
-        label.text(function(d) { return d.name; });
+        label.text(d => d.name);
       }
 
-      var hierarchy = function() {
-        link.attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
+      const hierarchy = () => {
+        link.attr("x1", d => d.source.x)
+            .attr("y1", d => d.source.y)
+            .attr("x2", d => d.target.x)
+            .attr("y2", d => d.target.y);
 
-        node.attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; });
+        node.attr("cx", d => d.x)
+            .attr("cy", d => d.y);
 
-        label.attr("x", function(d) { return d.x; })
-             .attr("y", function(d) { return d.y; });
+        label.attr("x", d => d.x)
+             .attr("y", d => d.y);
        };
 
-      var team = function(e) {
-        var k = 6 * e.alpha;
+      const team = e => {
+        const k = 6 * e.alpha;
         function getAnchor(team, dir) {
           if (dir == 'x') {
             result = ['Web', 'Core'].indexOf(team) > -1 ? true : false;
@@ -137,47 +135,47 @@
           return result;
         }
 
-        graph.nodes.forEach(function(o, i) {
+        graph.nodes.forEach((o, i) => {
           o.y += getAnchor(o.team, 'y') ? k : -k;
           o.x += getAnchor(o.team, 'x') ? k : -k;
         });
 
-        labels.forEach(function(o, i) {
+        labels.forEach((o, i) => {
           o.y += getAnchor(o.name, 'y') ? k : -k;
           o.x += getAnchor(o.name, 'x') ? k : -k;
         });
 
-        node.attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; });
+        node.attr("cx", d => d.x)
+            .attr("cy", d => d.y);
 
-        label.attr("x", function(d) { return d.x; })
-            .attr("y", function(d) { return d.y; });
+        label.attr("x", d => d.x)
+            .attr("y", d => d.y);
       };
 
-      var project = function(e) {
-        var k = 0.1 * e.alpha;
-        graph.nodes.forEach(function(o, i) {
+      const project = e => {
+        const k = 0.1 * e.alpha;
+        graph.nodes.forEach((o, i) => {
           o.y += (fociMap[o.project].y - o.y) * k;
           o.x += (fociMap[o.project].x - o.x) * k;
         });
 
-        labels.forEach(function(o, i) {
+        labels.forEach((o, i) => {
           o.y += (fociMap[o.name].y - o.y) * k;
           o.x += (fociMap[o.name].x + 100 - o.x) * k;
         });
 
         node
-            .attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; });
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y);
         label
-            .attr("x", function(d) { return d.x; })
-            .attr("y", function(d) { return d.y; });
-      }
+            .attr("x", d => d.x)
+            .attr("y", d => d.y);
+      };
 
-      var forceFns = [team, hierarchy, project];
-      var initForceFns = [initTeam, initHierarchy, initProject];
-      var forceFnIndex = 1;
-      var forceFn = forceFns[forceFnIndex];
+      const forceFns = [team, hierarchy, project];
+      const initForceFns = [initTeam, initHierarchy, initProject];
+      let forceFnIndex = 1;
+      let forceFn = forceFns[forceFnIndex];
 
       initForceFns[forceFnIndex]();
       force.on("tick", forceFn);
@@ -221,11 +219,11 @@
     }
 
     function getFociMap(items) {
-      var map = {};
-      var baseX = 300;
-      var baseY = 50;
-      var dy = 60;
-      var dx = 0;
+      const map = {};
+      const baseX = 300;
+      const baseY = 50;
+      const dy = 60;
+      const dx = 0;
 
       for(i=0; i<items.length; i++) {
         y = baseY + (i*dy);
@@ -246,7 +244,7 @@
     }
 
     function getLinks(indexMap, nameMap, items) {
-      var links = [];
+      const links = [];
       for(i=0; i<items.length; i++) {
         source = nameMap[items[i].name];
         target = nameMap[items[i].manager];
